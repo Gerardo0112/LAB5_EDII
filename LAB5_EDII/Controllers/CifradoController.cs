a4035ab847a1895cccbb404be8e5edbb5a98d360
Gerardo0112 <gerar0112@gmail.com>
2020-11-06 15:30:15 -0600

    Cifrado Ruta finalizado

--- LAB5_EDII/Controllers/CifradoController.cs
+++ LAB5_EDII/Controllers/CifradoController.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Net.Mime;
+using LAB5_EDII.Models;
+
 
 namespace LAB5_EDII.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class CifradoController : ControllerBase
     {
         
     }
--- LAB5_EDII/Models/Route.cs
+++ LAB5_EDII/Models/Route.cs
@@ -93,5 +93,103 @@ namespace LAB5_EDII.Models
                 }
             }
         }
+
+        //Decifrado Ruta Espiral
+        public static void Decipher(NumbersDataTaken info)
+        {
+            using (var reader = new BinaryReader(info.File.OpenReadStream()))
+            {
+                //Creacion archivo .txt.
+                using (var streamWriter = new FileStream($"{info.Name}.txt", FileMode.OpenOrCreate))
+                {
+                    using (var writer = new BinaryWriter(streamWriter))
+                    {
+                        //Buffer de lectura.
+                        var bufferLength = info.rows * info.columns;
+                        var byteBuffer = new byte[bufferLength];
+
+                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        {
+                            var Matriz = new byte[info.rows, info.columns];
+                            byteBuffer = reader.ReadBytes(bufferLength);
+                            var cont = 0;
+
+                            for (int i = 0; i < info.rows; i++)
+                            {
+                                for (int j = 0; j < info.columns; j++)
+                                {
+                                    if (cont < byteBuffer.Count())
+                                    {
+                                        Matriz[i, j] = byteBuffer[cont];
+                                        cont++;
+                                    }
+                                    else
+                                    {
+                                        Matriz[i, j] = (byte)0;
+                                    }
+                                }
+                            }
+
+                            var numVueltas = 0;
+                            var posX = 0;
+                            var posY = 0;
+                            var Direccion = "abajo";
+
+                            for (int i = 0; i < bufferLength; i++)
+                            {
+                                if (Matriz[posY, posX] != 0)
+                                {
+                                    if (Direccion == "abajo" && posY != info.rows - 1 - numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posY++;
+                                    }
+                                    else if (Direccion == "abajo" && posY == info.rows - 1 - numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posX++;
+                                        Direccion = "derecha";
+                                    }
+                                    else if (Direccion == "derecha" && posX != info.columns - 1 - numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posX++;
+                                    }
+                                    else if (Direccion == "derecha" && posX == info.columns - 1 - numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posY--;
+                                        Direccion = "arriba";
+                                    }
+                                    else if (Direccion == "arriba" && posY != numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posY--;
+                                    }
+                                    else if (Direccion == "arriba" && posY == numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        numVueltas++;
+                                        posX--;
+                                        Direccion = "izquierda";
+                                    }
+                                    else if (Direccion == "izquierda" && posX != numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posX--;
+                                    }
+                                    else if (Direccion == "izquierda" && posX == numVueltas)
+                                    {
+                                        writer.Write(Matriz[posY, posX]);
+                                        posY++;
+                                        Direccion = "abajo";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
