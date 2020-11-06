678119c20072415b5c0643998fc27072134136da
Gerardo0112 <gerar0112@gmail.com>
2020-11-06 00:18:04 -0600

    Creacion de clases y modificacion de API

---
+++ LAB5_EDII/API_CIFRADO/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LAB5_EDII.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
